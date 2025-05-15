07f71265c416f428680f016280009f4dbfa28694
petrinka <nikitaboss102@gmail.com>
2025-05-15 03:32:39 +0300

    add troppi plugin

---
+++ GraphEditor/Plugins/TROPPI.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using System;
+using System.Drawing;
+using Lab1;
+
+
+public class TROPPI : CommonArray
+    {
+        public TROPPI(Color penColor, Color brushColor, int penWidth, Point[] points)
+           : base(penColor, brushColor, penWidth, points) { }
+
+    
+
+    Point[] GetTrapezoid(Point start, Point end)
+    {
+        int offsetX = (end.X - start.X) / 4;
+        return new Point[]
+        {
+                new Point(start.X, end.Y),    // Нижний левый
+        new Point(start.X + offsetX, start.Y),           // Верхний левый
+        new Point(end.X - offsetX, start.Y),             // Верхний правый
+        new Point(end.X, end.Y)                    // Нижний правый
+
+        };
+    }
+
+
+
+    public override void Draw(Graphics g)
+        {
+        var resultPoints = GetTrapezoid(points[0], points[^1]);
+
+        using (var brush = new SolidBrush(brushColor))
+            using (var pen = new Pen(penColor, penWidth))
+            {
+
+                g.FillPolygon(brush, resultPoints);
+                g.DrawPolygon(pen, resultPoints);
+            }
+
+        }
+    }
+
---
+++ GraphEditor/Plugins/TROPPIBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Lab1;
+using System;
+using System.Drawing;
+using Lab1.classes.Builders.Interfaces;
+using System.Collections.Generic;
+
+
+
+    public class TROPPIBuilder : IFigureBuilder
+    {
+   
+        public bool isCreated { get; set; } = false;
+
+        private TROPPI troppi;
+        private Point start;
+        private Color lineColor, fillColor;
+        private int penWidth;
+
+        public void OnMouseDown(Point start, ref CommonArray[] shapes, Color lineColor, Color backColor, int penWidth)
+        {
+
+
+        this.start = start;
+            this.lineColor = lineColor;
+            fillColor = backColor;
+            this.penWidth = penWidth;
+            isCreated = true;
+            
+
+            troppi = new TROPPI(lineColor, backColor, penWidth, new Point[] { start, start });
+            Array.Resize(ref shapes, shapes.Length + 1);
+            shapes[shapes.Length - 1] = troppi;
+
+        }
+
+        public void OnMouseMove(Point current, ref CommonArray[] shapes, bool isDrawing)
+        {
+       
+        
+            shapes[^1] = new TROPPI(lineColor, fillColor, penWidth, new Point[] { start, current });
+        }
+
+    public void OnMouseUp(Point end, ref CommonArray[] shapes)
+    {
+        
+
+        shapes[^1] = new TROPPI(lineColor, fillColor, penWidth, new Point[] { start, end });
+    }
+
+
+
+
+}
